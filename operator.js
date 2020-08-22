ab987c62526887ce46ea00146bd61d5517758c88
Baejiu <57855548+Baejiu@users.noreply.github.com>
2020-08-22 15:01:00 +0900

    JS: operator

--- operator.js
+++ operator.js
@@ -82,5 +82,129 @@ function check() {
 }
 
 
-// ! (not)
-console.log(!value1);
+// ! (not) - 값을 반대로 바꿔줌
+console.log(!value1); 
+
+ // 7. Equality
+ const stringFive = '5';
+ const numberFive = 5;
+
+ // == loose equality, with type conversion
+ console.log(stringFive == numberFive);
+ console.log(stringFive != numberFive);
+
+ // === strict equality, no type conversion
+ console.log(stringFive === numberFive);
+ console.log(stringFive !== numberFive);
+
+  // object equality by reference
+  const ellie1 = { name: 'ellie' };
+  const ellie2 = { name: 'ellie' };
+  const ellie3 = ellie1;
+console.log(ellie1 == ellie2);
+console.log(ellie1 === ellie2);
+console.log(ellie1 === ellie3);
+
+ // equality - puzzler
+ console.log(0 == false);
+ console.log(0 === false);
+ console.log('' == false);
+ console.log('' === false);
+ console.log(null == undefined);
+ console.log(null === undefined);
+
+  // 8. Conditional operators: if
+  // if, else if, else
+  const name = 'ellie';
+  if (name==='ellie') {
+    console.log('Welcome, Ellie!');
+  } else if(name === 'coder') {
+    console.log('You are amazing coder');
+  } else {
+    console.log('unkwnon');
+  }
+
+   //9. Ternary operator: ?
+   //( condition ? value1 : value2 );
+   console.log(name === 'ellie' ? 'yes' : 'no' );
+
+    //10. Switch statement
+    // use for multiple if checks
+    // use for enum-like value check
+    // use for multiple type checks in TS
+    const browser = 'Chrome';
+    switch (browser) {
+      case 'IE':
+        console.log('go away!');
+        break;
+      case 'Chrome':
+        console.log('love you!');
+        break;
+      case 'Firefox':
+        console.log('love you!');
+        break;
+      default:
+        console.log('same all!');
+        break;
+    }
+    //case 결과값이 중복일 시 연달아 붙혀놓으면 됨
+    switch (browser) {
+      case 'IE':
+        console.log('go away!');
+        break;
+      case 'Chrome':
+      case 'Firefox':
+        console.log('love you!');
+        break;
+      default:
+        console.log('same all!');
+        break;
+    }
+
+    //11. Loops
+    // while loop, while the condition is truthy,
+    // body code is executed.
+    let i = 3;
+    while (i>0) {
+      console.log(`while: ${i}`);
+      i--;
+    }
+
+    //do while loop, body code is executed first,
+    // then check the condition.
+    do {
+      console.log(`do while: ${i}`);
+      i--;
+    } while (i>0);
+
+    //for loop, for(begin; condition; step)
+    for (i=3; i>0; i--) {
+      console.log(`for: ${i}`);
+    }
+
+    for (let i = 3; i > 0; i = i - 2 ) {
+      // inline variable declaration
+      console.log(`inline variable for: ${i}`);
+    }
+
+    //nested loops
+for (let i = 0; i < 10; i++) {
+  for (let j=0; j<10; j++){
+    console.log(`i: ${i}, j: ${j}`);
+  }
+}
+
+//Q1
+for (let a = 0; a < 11; a++) {
+  if (a % 2 === 0) {
+    console.log(a);
+  }
+}
+
+for (let b = 0; b < 10; b++) {
+  if (b > 8) {
+    break;
+  }
+  console.log(b);
+  
+}
